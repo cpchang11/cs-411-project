104701ed76b0c50349b56dcc66934856864011be
bgzhang <47161442+bgzhang@users.noreply.github.com>
2019-12-05 23:21:07 -0500

    Update index.js

--- assignment3/routes/index.js
+++ assignment3/routes/index.js
@@ -32,4 +32,121 @@ router.get('/api', async (request, response) => {
   response.json(json);
 });
 
+
+router.get('/api', async (request, response) => {
+  var playlistID = "";
+  const cuisineName = request.body.cuisines; 
+
+  if (cuisineName = "African")
+  {
+    playlistID = "37i9dQZEVXbMH2jvi6jvjk";  
+  }
+  if (cuisineName = "American")
+  {
+    playlistID = "37i9dQZEVXbLRQDuF5jeBp";
+  }
+  if (cuisineName = "British")
+  {
+    playlistID = "6POgRkMqqAScEncQQF3OWo";
+  }
+  if (cuisineName = "Cajun")
+  {
+    playlistID = "2KmefvmYGgMH8fvkHLWHse";
+  }
+  if (cuisineName = "Caribbean")
+  {
+    playlistID = "1m3tG3nrS5iNB8BEG6B4IT";
+  }
+  if (cuisineName = "Chinese")
+  {
+    playlistID = "3dtU7mRvUByjsR0p7IfrEg";
+  }
+  if (cuisineName = "Eastern European")
+  {
+    playlistID = "2Kltck0I6zDePUAG8RYBxs";
+  }
+  if (cuisineName = "European")
+  {
+    playlistID = "19RAb34RkjlayagyBiTsJV";
+  }
+  if (cuisineName = "French")
+  {
+    playlistID = "37i9dQZEVXbIPWwFssbupI";
+  }
+  if (cuisineName = "German")
+  {
+    playlistID = "37i9dQZEVXbJiZcmkrIHGU";
+  }
+  if (cuisineName = "Greek")
+  {
+    playlistID = "37i9dQZEVXbJqdarpmTJDL";
+  }
+  if (cuisineName = "Indian")
+  {
+    playlistID = "37i9dQZEVXbLZ52XmnySJg";
+  }
+  if (cuisineName = "Irish")
+  {
+    playlistID = "37i9dQZEVXbKM896FDX8L1";
+  }
+  if (cuisineName = "Italian")
+  {
+    playlistID = "37i9dQZEVXbIQnj7RRhdSX";
+  }
+  if (cuisineName = "Japanese")
+  {
+    playlistID = "37i9dQZEVXbKXQ4mDTEBXq";
+  }
+  if (cuisineName = "Jewish")
+  {
+    playlistID = "6mr1V7Fff6S512YvwnBqwm";
+  }
+  if (cuisineName = "Korean")
+  {
+    playlistID = "37i9dQZF1DX9tPFwDMOaN1";
+  }
+  if (cuisineName = "Latin American")
+  {
+    playlistID = "37i9dQZF1DX10zKzsJ2jva";
+  }
+  if (cuisineName = "Mediterranean")
+  {
+    playlistID = "3pS1vZCG1Xu2h2yFGWoqol";
+  }
+  if (cuisineName = "Mexican")
+  {
+    playlistID = "37i9dQZEVXbO3qyFxbkOE1";
+  }
+  if (cuisineName = "Middle Eastern")
+  {
+    playlistID = "5E7yzLgfs3WyEtvJtjmLPA";
+  }
+  if (cuisineName = "Nordic")
+  {
+    playlistID = "4r2twUYtviwpCpFnrfkkgM";
+  }
+  if (cuisineName = "Southern")
+  {
+    playlistID = "37i9dQZF1DXbDjX0hus3Iu";
+  }
+  if (cuisineName = "Spanish")
+  {
+    playlistID = "37i9dQZEVXbNFJfN1Vw8d9";
+  }
+  if (cuisineName = "Thai")
+  {
+    playlistID = "37i9dQZEVXbMnz8KIWsvf9";
+  }
+  if (cuisineName = "Vietnamese")
+  {
+    playlistID = "37i9dQZEVXbLdGSmz6xilI";
+  }
+
+
+
+
+
+
+});
+
 module.exports = router;
